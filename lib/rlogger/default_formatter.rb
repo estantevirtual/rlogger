1abbe13f6095cd3d643cc6824879165d6307c2e5
Pedro Rosanes <prosanes@gmail.com>
2016-01-11 16:22:00 -0200

    Adiciona pid e thread id ao formatter

--- lib/rlogger/default_formatter.rb
+++ lib/rlogger/default_formatter.rb
@@ -2,7 +2,7 @@ module RLogger
   class DefaultFormatter < Logger::Formatter
     def call(severity, time, progname, msg)
       hostname = Socket.gethostname
-      "[#{time}] [Hostname: #{hostname} | Service: #{progname}] (#{severity}): #{msg}\n"
+      "#{severity[0]}, [#{time}] [PID: ##{$$} Thread: ##{Thread.current.object_id}] [Hostname: #{hostname} | Service: #{progname}] (#{severity}): #{msg}\n"
     end
   end
 end
